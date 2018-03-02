a50afa80a2026b10ba2e717e45c25b54376a7f40
ufdilla <learnmore.cumi@gmail.com>
2018-03-02 17:01:22 +0700

    tambah konektor

---
+++ app/src/main/java/com/example/easysoft/kaiwaapp/ConnectorActivity.java
@@ -0,0 +1,63 @@
+package com.example.easysoft.kaiwaapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConnectorActivity extends AsyncTask<Void, Void, Void> {
+
+    TextView textResponse1;
+//    String textDestination;
+//    String username;
+    String textRequest;
+    String message = "";
+    Socket socket;
+//    myModel model;
+
+    public ConnectorActivity(Socket socket, String textRequest, TextView textViewResponse) {
+        this.socket = socket;
+        this.textRequest = textRequest;
+        this.textResponse1= textViewResponse;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            JSONObject jObj = new JSONObject();
+            jObj.put("message", textRequest);
+//            jObj.put("username", username);
+//            jObj.put("destination", textDestination);
+            dataOutputStream.writeUTF(String.valueOf(jObj));
+            dataOutputStream.flush();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            message = "UnknownHostException: " + e.toString() + "\r\n";
+        } catch (IOException e) {
+            e.printStackTrace();
+            message = "IOException: " + e.toString() + "\r\n";
+            e.getCause();
+            e.getMessage();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            e.getCause();
+            e.getMessage();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+    }
+}
